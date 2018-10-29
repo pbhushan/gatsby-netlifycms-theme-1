8b4e810e9291ba62368177db17f4b3f8fb753dec
Puspendu <pb249@njit.edu>
2018-10-29 15:36:38 +0530

    contact link added in navbar

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -1,49 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
 
 export const ContactPageTemplate = ({ title }) => (
   <section className="section section--gradient">
-                <h2
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                    padding: '1rem',
-                  }}
-                >
-                  {title}
-                </h2>            
+    <h2
+      className="has-text-weight-bold is-size-2"
+      style={{
+        boxShadow: "0.5rem 0 0 #f40, -0.5rem 0 0 #f40",
+        backgroundColor: "#f40",
+        color: "white",
+        padding: "1rem"
+      }}
+    >
+      {title}
+    </h2>
   </section>
-)
+);
 
 ContactPageTemplate.propTypes = {
   title: PropTypes.string
-}
+};
 
 const ContactPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
-      <ContactPageTemplate
-        title={frontmatter.title}
-      />
+      <ContactPageTemplate title={frontmatter.title} />
     </Layout>
-  )
-}
+  );
+};
 
 ContactPage.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.object,
-    }),
-  }),
-}
+      frontmatter: PropTypes.object
+    })
+  })
+};
 
-export default ContactPage
+export default ContactPage;
 
 export const ContactPageQuery = graphql`
   query ContactPage($id: String!) {
@@ -53,4 +51,4 @@ export const ContactPageQuery = graphql`
       }
     }
   }
-`
+`;
